f04d0fedd6811c45d4123cd8bd537014381cb260
yezhaoliang <zhaoturkkey@163.com>
2022-02-06 02:06:02 +0800

    utils

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+# *
+# *  @author Zhaoliang Ye 叶昭良(zl_ye@qny.chng.com.cn)
+# *  @version V0.1
+# *  @Title: utils.py
+# *  @Description: 常用工具函数组合
+# *  @Time: 2022/2/6 1:35
+# *
+
+class Util(object):
+    """
+    知识点： 通过静态方法 封装常用工具类
+    """
+    @staticmethod
+    def md5my(self):
+        pass
